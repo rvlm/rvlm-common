b89d9317f42554cdf4e81d1e5f20da8a9ad8fe1c
Pavel Kretov <firegurafiku@gmail.com>
2014-06-15 14:30:47 +0400

    Math.hh: cosmetic changes
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- rvlm-core/include/rvlm/core/Math.hh
+++ rvlm-core/include/rvlm/core/Math.hh
@@ -10,8 +10,10 @@ namespace math {
  * Please note that despite its name this function returns a floating point
  * number, not integral. Just like @c std::floor and @c std::ceil, though.
  */
-template <typename T>
-inline T round(const T& x) { return std::floor(x+static_cast<T>(1)/2); }
+template <typename TFloat>
+inline TFloat round(const TFloat& x) {
+    return std::floor(x+static_cast<TFloat>(1)/2);
+}
 
 } // namespace math
 } // namespace core

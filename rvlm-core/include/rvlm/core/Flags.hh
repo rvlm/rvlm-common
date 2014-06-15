4b35a4dd73ad5bfa855b5dfb2168a4cef2719c55
Pavel Kretov <firegurafiku@gmail.com>
2014-06-15 14:36:49 +0400

    Flags.hh: remove TInt generic parameter
    Having two generic parameters Flags<TEnum,TInt> made it impossible to
    create flags from constants combination:
    
      Enum::k1 | Enum::k2 -> Flags<Enum, TInt>
      - What type TInt must be? Impossible to infer for compiler.
    
    So, it was desided to stick to std::uint_fast32_t for every enumeration
    which should be enough for almost every application.
    
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- rvlm-core/include/rvlm/core/Flags.hh
+++ rvlm-core/include/rvlm/core/Flags.hh
@@ -8,32 +8,8 @@ namespace core {
 /**
  *
  */
-template<
-    typename TEnum,
-    typename TInt = std::uint_fast32_t>
+template<typename TEnum>
 struct Flags {
-private:
-    /**
-     * @internal
-     * Raw bitmask value of enumeration flags.
-     * The rest of this class does nothing more than simply wraps this value
-     * with convenience operators and methods, with hope they will be optimized
-     * and inlined out by the compiler, leading to machine code accessing this
-     * value directly without any overhead.
-     */
-    TInt value;
-
-    /**
-     * @internal
-     * Returns class instance with raw value set to @a val.
-     * This method is heavily used for implementing operators on sets.
-     */
-    static Flags<TEnum,TInt> fromValue(TInt val) {
-        auto result = Flags<TEnum,TInt>();
-        result.value = val;
-        return result;
-    }
-
 public:
 
     /**
@@ -46,7 +22,7 @@ public:
      * Convenience alias for underlying integer type implementing bitmask.
      * This may be useful in some rare situations.
      */
-    typedef TInt IntType;
+    typedef std::uint_fast32_t IntType;
 
     /**
      * Constructs empty set of flags.
@@ -55,11 +31,10 @@ public:
         : value(0) {}
 
     /**
-     * Constructs set with single flag @c item included.
+      Constructs set with single flag @c item included.
      */
     Flags(const TEnum& item)
-        : value(static_cast<TInt>(1) <<
-                static_cast<std::underlying_type<TEnum>::type>(item)) {}
+        : value(static_cast<IntType>(1) << static_cast<IntType>(item)) {}
 
     /**
      * Includes flag for @a item into this set.
@@ -104,7 +79,7 @@ public:
     /**
      *
      */
-    Flags<TEnum,TInt>& operator &= (const Flags<TEnum,TInt>& other) {
+    Flags<TEnum>& operator &= (const Flags<TEnum>& other) {
         return value &= other.value;
         return *this;
     }
@@ -112,7 +87,7 @@ public:
     /**
      *
      */
-    Flags<TEnum,TInt>& operator |= (const Flags<TEnum,TInt>& other) {
+    Flags<TEnum>& operator |= (const Flags<TEnum>& other) {
         value |= other.value;
         return *this;
     }
@@ -120,7 +95,7 @@ public:
     /**
      *
      */
-    Flags<TEnum,TInt>& operator ^= (const Flags<TEnum,TInt>& other) {
+    Flags<TEnum>& operator ^= (const Flags<TEnum>& other) {
         value ^= other.value;
         return *this;
     }
@@ -128,21 +103,21 @@ public:
     /**
      *
      */
-    Flags<TEnum,TInt>& operator *= (const Flags<TEnum,TInt>& other) {
+    Flags<TEnum>& operator *= (const Flags<TEnum>& other) {
         return *this &= other;
     }
 
     /**
      *
      */
-    Flags<TEnum,TInt>& operator += (const Flags<TEnum,TInt>& other) {
+    Flags<TEnum>& operator += (const Flags<TEnum>& other) {
         return *this |= other;
     }
 
     /**
      *
      */
-    Flags<TEnum,TInt>& operator -= (const Flags<TEnum,TInt>& other) {
+    Flags<TEnum>& operator -= (const Flags<TEnum>& other) {
         value &= value ^ other.value;
         return *this;
     }
@@ -150,106 +125,124 @@ public:
     /**
      *
      */
-    Flags<TEnum,TInt> operator & (const Flags<TEnum,TInt>& other) const {
-        return Flags<TEnum,TInt>::fromValue(value & other.value);
+    Flags<TEnum> operator & (const Flags<TEnum>& other) const {
+        return Flags<TEnum>::fromValue(value & other.value);
     }
 
     /**
      *
      */
-    Flags<TEnum,TInt> operator | (const Flags<TEnum,TInt>& other) const {
-        return Flags<TEnum,TInt>::fromValue(value | other.value);
+    Flags<TEnum> operator | (const Flags<TEnum>& other) const {
+        return Flags<TEnum>::fromValue(value | other.value);
     }
 
     /**
      *
      */
-    Flags<TEnum,TInt> operator ^ (const Flags<TEnum,TInt>& other) const {
-        return Flags<TEnum,TInt>::fromValue(value ^ other.value);
+    Flags<TEnum> operator ^ (const Flags<TEnum>& other) const {
+        return Flags<TEnum>::fromValue(value ^ other.value);
     }
 
     /**
      *
      */
-    Flags<TEnum,TInt> operator * (const Flags<TEnum,TInt>& other) const {
-        return Flags<TEnum,TInt>::fromValue(value & other.value);
+    Flags<TEnum> operator * (const Flags<TEnum>& other) const {
+        return Flags<TEnum>::fromValue(value & other.value);
     }
 
     /**
      *
      */
-    Flags<TEnum,TInt> operator + (const Flags<TEnum,TInt>& other) const {
-        return Flags<TEnum,TInt>::fromValue(value | other.value);
+    Flags<TEnum> operator + (const Flags<TEnum>& other) const {
+        return Flags<TEnum>::fromValue(value | other.value);
     }
 
     /**
      *
      */
-    Flags<TEnum,TInt> operator - (const Flags<TEnum,TInt>& other) const {
-        return Flags<TEnum,TInt>::fromValue(value & (value ^ other.value));
+    Flags<TEnum> operator - (const Flags<TEnum>& other) const {
+        return Flags<TEnum>::fromValue(value & (value ^ other.value));
     }
 
     /**
      *
      */
-    bool operator == (const Flags<TEnum,TInt>& other) const {
+    bool operator == (const Flags<TEnum>& other) const {
         return value == other.value;
     }
 
     /**
      *
      */
-    bool operator != (const Flags<TEnum,TInt>& other) const {
+    bool operator != (const Flags<TEnum>& other) const {
         return value != other.value;
     }
 
     /**
      *
      */
-    bool operator <= (const Flags<TEnum,TInt>& other) const {
+    bool operator <= (const Flags<TEnum>& other) const {
         return *this | other == other;
     }
 
     /**
      *
      */
-    bool operator < (const Flags<TEnum,TInt>& other) const {
+    bool operator < (const Flags<TEnum>& other) const {
         return *this <= other && *this != other;
     }
 
     /**
      *
      */
-    bool operator >= (const Flags<TEnum,TInt>& other) const {
+    bool operator >= (const Flags<TEnum>& other) const {
         return other <= *this;
     }
 
     /**
      *
      */
-    bool operator > (const Flags<TEnum,TInt>& other) const {
+    bool operator > (const Flags<TEnum>& other) const {
         return other < *this;
+    }
+
+private:
+    /**
+     * @internal
+     * Raw bitmask value of enumeration flags.
+     * The rest of this class does nothing more than simply wraps this value
+     * with convenience operators and methods, with hope they will be optimized
+     * and inlined out by the compiler, leading to machine code accessing this
+     * value directly without any overhead.
+     */
+    IntType value;
+
+    /**
+     * @internal
+     * Returns class instance with raw value set to @a val.
+     * This method is heavily used for implementing operators on sets.
+     */
+    static Flags<TEnum> fromValue(const IntType val) {
+        auto result = Flags<TEnum>();
+        result.value = val;
+        return result;
     }
 };
 
 /**
  *
  */
-template<
-    typename TEnum,
-    typename TInt>
-Flags<TEnum,TInt> operator | (const TEnum& a, const TEnum& b) {
-    return Flags<TEnum,TInt>(a) | b;
+template<typename TEnum>
+Flags<TEnum> operator | (const TEnum& a, const TEnum& b) {
+    return Flags<TEnum>(a) | b;
 }
 
 /**
  *
  */
-template<
-    typename TEnum,
-    typename TInt>
-Flags<TEnum,TInt> operator + (const TEnum& a, const TEnum& b) {
-    return Flags<TEnum,TInt>(a) + b;
+template<typename TEnum>
+Flags<TEnum> operator + (const TEnum& a, const TEnum& b) {
+    return Flags<TEnum>(a) + b;
 }
 
 } // namespace core
